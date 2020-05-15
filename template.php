ab64c1572c6ec4bef625e0532bcb186822595bb9
Ronnyriyana <riyanaronny@gmail.com>
2020-05-15 20:45:26 +0700

    oprek tampilan 2

--- template.php
+++ template.php
@@ -9,11 +9,11 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <html lang="zxx">
 
 <head>
-    <title>Medizin Medical Category Bootstrap Responsive Web Template | Home :: W3layouts</title>
+    <title>The Heart Saver</title>
     <!-- Meta tag Keywords -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8" />
-    <meta name="keywords" content="Medizin Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <meta name="keywords" content="Monitoring Denyut Nadi, Posisi Pasien" />
     <script>
         addEventListener("load", function() {
             setTimeout(hideURLbar, 0);
@@ -32,12 +32,13 @@ License URL: http://creativecommons.org/licenses/by/3.0/
     <link rel="stylesheet" href="css/slider.css" type="text/css" media="all" />
     <!-- Style-CSS -->
     <!-- font-awesome-icons -->
-    <link href="css/font-awesome.css" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/dd238ac0b8.js" crossorigin="anonymous"></script>
     <!-- //font-awesome-icons -->
     <!-- /Fonts -->
     <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700" rel="stylesheet">
     <link href="//fonts.googleapis.com/css?family=Dosis:200,300,400,500,600,700" rel="stylesheet">
     <!-- //Fonts -->
+	
 </head>
 
 <body>
@@ -51,35 +52,17 @@ License URL: http://creativecommons.org/licenses/by/3.0/
                         <div class="header-section">
                             <h1>
                                 <a class="navbar-brand logo editContent" href="index.html">
-                                    <span class="fa fa-heartbeat"></span>Saver
+                                    <span class="fas fa-heartbeat" ></span> Saver
                                 </a>
                             </h1>
                         </div>
                         <div class="nav_section">
                             <nav>
-                                <label for="drop" class="toggle mt-lg-0 mt-1"><span class="fa fa-bars" aria-hidden="true"></span></label>
+                                <label for="drop" class="toggle mt-lg-0 mt-1"><span class="fas fa-bars" aria-hidden="true"></span></label>
                                 <input type="checkbox" id="drop" />
                                 <ul class="menu">
-                                    <li class="active"><a href="index.html">Home</a></li>
-                                    <li><a href="about.html">About </a></li>
-                                    <li>
-                                        <!-- First Tier Drop Down -->
-                                        <label for="drop-2" class="toggle">Dropdown <span class="fa fa-angle-down" aria-hidden="true"></span> </label>
-                                        <a href="#">Dropdown <span class="fa fa-angle-down" aria-hidden="true"></span></a>
-                                        <input type="checkbox" id="drop-2" />
-                                        <ul class="inner-dropdown">
-                                            <li><a href="#stats">Stats</a></li>
-                                            <li><a href="#lab">Laboratory</a></li>
-                                            <li><a href="#test">Reviews</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="gallery.html">Departments</a></li>
-                                    <li><a href="contact.html">Contact Us</a></li>
-                                    <li class="icons">
-                                        <a href="#" class="face-bk"><span class="fa fa-facebook" aria-hidden="true"></span></a></li>
-
-                                    <li class="icons"><a href="#" class="twitter"><span class="fa fa-twitter" aria-hidden="true"></span></a></li>
-                                    <li class="icons"><a href="#" class="dribble"><span class="fa fa-dribbble"></span></a></li>
+                                    <li class="active"><a href="#">Monitoring </a></li>
+                                    <li><a href="#">Log Aktivitas </a></li>
 
                                 </ul>
                             </nav>
@@ -111,30 +94,26 @@ License URL: http://creativecommons.org/licenses/by/3.0/
         <!-- //header -->
         <section class="banner-bottom py-5">
             <div class="container py-md-5">
-                <div class="row mt-lg-5 mt-4 pt-5 text-center" id="stats">
+                <div class="row mt-lg-5 text-center" id="stats">
 
                     <div class="col-lg-4 counter editContent mt-3">
-                        <span class="fa fa-users"></span>
-                        <div class="counter-info">
-                            <h4>7200+</h4>
-                            <p>Healthy and happy customers</p>
-
-                        </div>
+							<button class="accordion">Nama : Fajar Adi</button>
+							<div class="panel">
+							  <p>Usia : 45 Thn.</p>
+							</div>
                     </div>
                     <div class="col-lg-4 counter two editContent mt-3">
-                        <span class="fa fa-user-md"></span>
+						<p>Denyut Nadi</p>
+                        <img src="images/Denyutnadi.png" width="30%" id="gmbrJantung">
                         <div class="counter-info">
-                            <h4>150+</h4>
-                            <p>Professional medical specialist</p>
-
+                            <h4>71 BPM</h4>
                         </div>
                     </div>
                     <div class="col-lg-4 counter editContent mt-3">
-                        <span class="fa fa-diamond"></span>
+						<p>Keberadaan Pasien</p>
+                        <span class="fa fa-layer-group" size="30%"></span>
                         <div class="counter-info">
-                            <h4>15+</h4>
-                            <p>Years of impeccable and successful work</p>
-
+                            <h4>Lantai 2</h4>
                         </div>
                     </div>
                 </div>
@@ -146,7 +125,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
         <div class="cpy-right py-3">
             <div class="container">
                 <div class="row">
-                    <p class="col-md-10 text-left">© 2019 Medizin. All rights reserved | Design by
+                    <p class="col-md-10 text-left">© 2020 Kronous. All rights reserved | Design by
                         <a href="http://w3layouts.com"> W3layouts.</a>
                     </p>
                     <!-- move top icon -->
@@ -159,6 +138,22 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 
 
     </div>
+	<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+</script>
 </body>
 
 </html>
