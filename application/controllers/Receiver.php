01ab2604d9bdcebcfe566d3d1104eb3c4dd985ac
Ronnyriyana <riyanaronny@gmail.com>
2020-07-13 22:17:25 +0700

    Receiver dengan email

--- application/controllers/Receiver.php
+++ application/controllers/Receiver.php
@@ -24,6 +24,11 @@ class Receiver extends CI_Controller {
 			$res = $this->receiver_m->insertLog($data);
 			if($res>=1){
 				echo "Berhasil";
+				if($data['denyut_nadi']<50){
+					send_mail("Danger","Pasien bernama tatang dalam kondisi berbahaya");
+				}elseif(($data['denyut_nadi']>100)){
+					send_mail("Need Attention","Pasien bernama tatang perlu perawatan");
+				}
 			}
 			else{
 				echo "Gagal";
@@ -31,7 +36,38 @@ class Receiver extends CI_Controller {
 		}else{
 			echo "Data dengan waktu tersebut sudah terkirim !";
 		}
-      }
+	  }
+	  
+	  public function send_mail($subject,$pesan) { 
+
+		$from_email = "support@theheartsaver.in"; 
+		$to_email = "riyanaronny@gmail.com";
+
+		$config = Array(
+			   'protocol' => 'smtp',
+			   'smtp_host' => 'ssl://mail.theheartsaver.in ',
+			   'smtp_port' => 465,
+			   'smtp_user' => $from_email,
+			   'smtp_pass' => 'SOL.~0A}?ldU',
+			   'mailtype'  => 'html', 
+			   'charset'   => 'iso-8859-1'
+	   );
+
+		   $this->load->library('email', $config);
+		   $this->email->set_newline("\r\n");   
+
+		$this->email->from($from_email, 'The Heart Saver'); 
+		$this->email->to($to_email);
+		$this->email->subject($subject); 
+		$this->email->message($pesan); 
+
+		//Send mail 
+		if($this->email->send()){
+			   Echo "</br> Email terkirim";
+		}else {
+			   Echo "</br> Email tidak terkirim";
+		} 
+	 }
       
 	
 }
